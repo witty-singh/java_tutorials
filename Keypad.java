9a5f202059a435ebcd5abc38c1765c14697ce362
Orpita4592 <92620425+Orpita4592@users.noreply.github.com>
2021-10-16 16:20:04 +0530

    Update Keypad.java

--- Keypad.java
+++ Keypad.java
@@ -67,9 +67,10 @@ public class Keypad {
 	}
 	public static void main(String[] args)throws Invalid_Input {
 		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the value of n");
 		int n = sc.nextInt();
 		String arr[] = keypad(n);
-		for (int i = 0; i < arr.length; i++)
+		for (int i = 0; i < arr.length; i++){
 			System.out.println(arr[i]);
-	}
+	}}
 }
